226bf4b91a00c51ccd2150a62f8c778ba558b94e
lolobster <nissinena@elisanet.fi>
2015-09-17 15:54:58 +0300

    villistipyöriviäneliöitäääääheitoimiiiiih

---
+++ kolomio/Pyöriviä neliöitä ebins/vertexshader.h~
@@ -0,0 +1,7 @@
+#ifndef __VERTEXSHADER_H__
+#define __VERTEXSHADER_H__
+
+#define VERTEX_POSITION 0
+#define VERTEX_COLOR 1
+
+#endif
---
+++ kolomio/Pyöriviä neliöitä ebins/vertexshaderx.h
@@ -0,0 +1,8 @@
+#ifndef __VERTEXSHADER_H__
+#define __VERTEXSHADER_H__
+
+//defined also in vertexshader
+#define VERTEX_POSITION 0
+#define TEXTURE_DATA 1
+
+#endif
